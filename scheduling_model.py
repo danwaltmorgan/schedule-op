48562a24ae07d37734237d7f873c3ac47fbd59bd
Dan Morgan <ltdan681@gmail.com>
2020-11-28 00:31:37 -0500

    safety

--- scheduling_model.py
+++ scheduling_model.py
@@ -7,16 +7,22 @@ Created on Sun Nov 22 17:13:02 2020
 
 from ortools.sat.python import cp_model
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def main():
     
+    np.random.seed(936)
+    
     #define the variables
-    residents = 5
+    residents = 4
     weeks = 4
     shifts = weeks * 21
+    
     total_residents = range(residents)
     total_shifts = range(shifts)
     shift_requests = np.random.randint(2, size = (residents, shifts)) 
+
     min_per_shift = 1
     max_per_shift = 2
 
@@ -63,6 +69,7 @@ def main():
     
     for r in total_residents:
         shift_model.Add(sum(x[r,s] for s in total_shifts) <= 40)
+        shift_model.Add(sum(x[r,s] for s in total_shifts) >= 35)
       
    
     #3+2 constraint
@@ -118,17 +125,19 @@ def main():
 #         print()
 # =============================================================================
         
-    for r in total_residents:
-        for s in total_shifts:
-            print(solver.Value(x[(r,s)]), end=(""))
-            
-        print()
-    for r in total_residents:
-        print()
-        for w in range(0, shifts, 21):
-            print()
-            for d in range(w, w+21, 3):
-                print(solver.Value(z[(r,w,d)]), end="")
+# =============================================================================
+#     for r in total_residents:
+#         for s in total_shifts:
+#             print(solver.Value(x[(r,s)]), end=(""))
+#             
+#         print()
+#     for r in total_residents:
+#         print()
+#         for w in range(0, shifts, 21):
+#             print()
+#             for d in range(w, w+21, 3):
+#                 print(solver.Value(z[(r,w,d)]), end="")
+# =============================================================================
     
     #Stats
     print()
@@ -136,6 +145,50 @@ def main():
     print('Number of requests met = %i' % solver.ObjectiveValue(),
           '(out of %i)' % num_shifts_req)
     print("Walltime: %f s" % solver.WallTime())
+    
+    #Grid plot of schedule
+    
+    shift_matrix = []
+    value_matrix = []
+    for r in total_residents:
+        shift_result = []
+        value = 0
+        tot_shifts = 0
+        for s in total_shifts:
+            if solver.Value(x[(r,s)]) == 1:
+                value = value + shift_requests[r][s]
+                tot_shifts = tot_shifts + 1
+                if shift_requests[r][s] == 1:
+                    shift_result.append((128,128,128))
+                else:
+                    shift_result.append((204,51,0))
+            else:
+                shift_result.append((224,224,224))
+        shift_matrix.append(shift_result)
+        value_matrix.append((r,tot_shifts,value))
+        
+    plt.figure(figsize = (8,3))
+    
+    plt.imshow(shift_matrix, aspect = 3)
+    plt.axvline(20.5, color = 'black')
+    plt.axvline(41.5, color = 'black')
+    plt.axvline(62.5, color = 'black')
+    
+    #add borders
+    
+    for r in total_residents:
+        for s in total_shifts:
+            rec = plt.Rectangle((s-0.5,r-0.5), 1, 1, 
+                                facecolor = 'none', 
+                                edgecolor = 'white', 
+                                linewidth=1)
+            plt.gca().add_patch(rec)
+                    
+    week_set = [10.5,31.5,52.5,73.5]
+    plt.tick_params(axis='both', bottom=False)
+    plt.xticks(week_set,['Week 1', 'Week 2','Week 3','Week 4'],fontsize=10)
+    plt.yticks([0,1,2,3],['R1','R2','R3','R4'],fontsize=8)
+    plt.tick_params(axis = "both", which = "both", bottom = False, left = False)
     
 
 if __name__ == '__main__':
