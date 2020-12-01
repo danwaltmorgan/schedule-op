8cf89b4fd6b15eb276982dd6c0005223f336ce81
Dan Morgan <ltdan681@gmail.com>
2020-11-30 23:09:03 -0500

    added social constraint but it interfers with the one resident from every year constraint

--- rotation_model.py
+++ rotation_model.py
@@ -32,7 +32,7 @@ years = [[0,1,2,3,4,5,6],[7,8,9,10,11,12,13],[14,15,16,17,18,19,20],[21,22,23,24
 
 
 # Initialize vacation preferences
-vac_pref = np.random.randint(low = 1, high = 4, size = (residents, blocks)) 
+vac_pref = np.random.randint(2, size = (residents, blocks)) 
         
 
 
@@ -52,6 +52,13 @@ for r in range(residents):
         for c in range(clinics):
             y[(r,b,c)] = rotation_model.NewBoolVar('y_%i%i%i' % (r, b, c))
 
+z = {}
+for i in range(residents):
+    for j in range(residents):
+        for b in range(blocks):
+            for c in range(clinics):
+        # z[(i,j)] = rotation_model.NewBoolVar('z_%i%i' % (i,j))
+                z[(i,j,b,c)] = rotation_model.NewBoolVar('z_%i%i%i%i' % (i,j,b,c))
 
 
 ################################################################################
@@ -65,19 +72,21 @@ for r in range(residents):
 for r in range(residents):
     for c in range(clinics):
         rotation_model.Add(sum(x[(r,b,c)] for b in range(blocks)) >= 1) 
-        rotation_model.Add(sum(x[(r,b,c)] for b in range(blocks)) <= 4)
+        rotation_model.Add(sum(x[(r,b,c)] for b in range(blocks)) <= 2)
 
 # c. Each clinic must have one resident from each year
-for b in range(blocks):
-    for c in range(clinics):
-        for year in years:
-            rotation_model.Add(sum(x[(y,b,c)] for y in year) == 1) 
+# =============================================================================
+# for b in range(blocks):
+#     for c in range(clinics):
+#         for year in years:
+#             rotation_model.Add(sum(x[(y,b,c)] for y in year) == 1) 
+# =============================================================================
 
 # d. Each resident must take four vacations
 for r in range(residents):
    rotation_model.Add(sum(y[(r,b,c)] 
             for b in range(blocks)
-            for c in range(clinics)) == 3)
+            for c in range(clinics)) == 2)
 
 # e. Hard clinics (5-7) should not be back-to-back
 for r in range(residents):
@@ -100,13 +109,27 @@ for r in range(residents):
 # h. Only two residents can take vacation within the same clinic in the same block
 for b in range(blocks):
     for c in range(clinics):
-        rotation_model.Add(sum(y[(r,b,c)] for r in range(residents)) <= 2)
+        rotation_model.Add(sum(y[(r,b,c)] for r in range(residents)) <= 4)
+        
+        
+for i in range(residents):
+    for j in range(residents):
+        works_tot = 0
+        for b in range(blocks):
+            for c in range(clinics):
+                rotation_model.Add(z[(i,j,b,c)] + 1 >= x[(i,b,c)] + x[(j,b,c)])
+                rotation_model.Add(2 * z[(i,j,b,c)] <= x[(i,b,c)] + x[(j,b,c)])
+                works_tot += z[(i,j,b,c)]
 
+        rotation_model.Add(sum(z[(i,j,b,c)] 
+                                for b in range(blocks) 
+                                for c in range(clinics)
+                               ) > 0)
 
 ################################################################################
 # 3. Objective Function
 # minimize vac pref * actual for each resident in each block
-rotation_model.Minimize(
+rotation_model.Maximize(
         sum(vac_pref[r][b] * y[(r,b,c)] 
             for r in range(residents)
             for b in range(blocks)
@@ -189,6 +212,15 @@ for r in range(residents):
             if solver.Value(x[(r,b,c)]) >= 1:
                 text = plt.text(b,r,c, ha = "center", va = "center", color = "black", fontsize = 12)
 
+
+for i in range(residents):
+    print()
+    for j in range(residents):
+       works_tot = 0
+       for b in range(blocks):
+           for c in range(clinics):
+               works_tot += solver.Value(z[(i,j,b,c)])
+       print(works_tot, end="")
 
 print("Solution = ", solver.ObjectiveValue())
 
